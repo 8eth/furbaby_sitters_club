6f9a2d256195d2ec46b65745eae48b0e8c434fea
Betelihem <Betelihem>
2022-04-19 10:04:49 -0700

    login works, stays logged in, signup works, calendar added
    Co-authored-by: krisperu <krisperu@users.noreply.github.com>
    Co-authored-by: Bevin Duncan <bevin-duncan@users.noreply.github.com>

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,11 +2,12 @@ import React from 'react'
 import NavBar from './NavBar'
 
 
-function LandingPage() {
+function LandingPage({sitters}) {
   return (
     <div>
 
       <NavBar/>
+      <SitterDetail />
     </div>
   )
 }
