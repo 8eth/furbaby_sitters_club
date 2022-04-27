bb317e3b11765bbee3b8d91f05875654f85857a4
Kristina Peru <krissy.germanov@gmail.com>
2022-04-27 14:40:07 -0600

    Added resource

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@
   post "/signup", to: "clients#create"
   post "/login", to: "sessions#login"
   delete "/logout", to: "sessions#logout"
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   
 end
